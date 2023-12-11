b9054c4f1838d2206a89c1b22dacad66e22cabbb
TBoileau <t-boileau@email.com>
2023-12-11 21:43:14 +0100

    fix: delete dead code

--- tests/Func/ImportGithubEventsCommandTest.php
+++ tests/Func/ImportGithubEventsCommandTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Tests\Func;
 
 use App\Entity\Event;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -23,28 +22,10 @@ final class ImportGithubEventsCommandTest extends KernelTestCase
 
         $commandTester->assertCommandIsSuccessful();
 
-        $events = $this->getEntityManager()->getRepository(Event::class)->findAll();
+        $entityManager = self::$kernel->getContainer()->get('doctrine')->getManager();
+
+        $events = $entityManager->getRepository(Event::class)->findAll();
 
         self::assertCount(96, $events);
-    }
-
-    private function truncateEntities(array $entities)
-    {
-        $connection = $this->getEntityManager()->getConnection();
-        $databasePlatform = $connection->getDatabasePlatform();
-
-        foreach ($entities as $entity) {
-            $query = $databasePlatform->getTruncateTableSQL(
-                $this->getEntityManager()->getClassMetadata($entity)->getTableName()
-            );
-            $connection->executeStatement($query . ' CASCADE');
-        }
-    }
-
-    private function getEntityManager(): EntityManagerInterface
-    {
-        return self::$kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
     }
 }
